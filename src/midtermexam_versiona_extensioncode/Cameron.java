870b702658529504517a3c31f763ea026afec9c9
Cameron Wilson <wilso211@sheridancollege.ca>
2023-02-24 12:26:04 -0500

    Created the java file and added comments as requested

--- src/midtermexam_versiona_extensioncode/Cameron.java
+++ src/midtermexam_versiona_extensioncode/Cameron.java
@@ -10,4 +10,64 @@ package midtermexam_versiona_extensioncode;
  */
 public class Cameron {
     //This is an added comment for the midterm
+    //Not sure what else I'm meant to put, so here's some more comments
+    /*
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+    euismod id mauris in mattis. Morbi ac sodales arcu. Nam tincidunt
+    ullamcorper semper. Sed aliquet velit risus, ac pulvinar lorem tincidunt
+    nec. Etiam iaculis, nisi quis dapibus venenatis, massa eros mollis neque,
+    sed commodo nulla nunc a erat. Vestibulum justo ante, vehicula vitae nibh a,
+    rutrum cursus ante. Ut faucibus neque sit amet lectus mollis tincidunt.
+    Phasellus magna mauris, tempor sit amet rutrum ac, pretium vitae neque.
+    Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+    ridiculus mus. Vestibulum posuere nec erat et hendrerit. Nam volutpat
+    congue nisi vel tempor. Fusce rutrum ullamcorper dolor, vel pulvinar arcu
+    venenatis at. Phasellus volutpat vestibulum cursus. Pellentesque sed
+    sodales nunc. Nam eget bibendum elit. Integer tempus luctus felis.
+
+    Praesent eleifend vulputate est tincidunt vulputate. Aenean venenatis diam
+    at lorem vestibulum, a tristique ipsum pellentesque. Praesent facilisis odio
+    at vulputate facilisis. Nulla facilisi. Vestibulum ac nunc eu nibh iaculis
+    condimentum ut rutrum erat. Aliquam nec est vitae nulla laoreet ultricies.
+    Curabitur quis tristique magna. Curabitur tincidunt, diam sit amet pharetra
+    condimentum, turpis ante suscipit magna, ut cursus enim dolor vitae metus.
+    Proin gravida, massa eget posuere placerat, turpis dui porta neque, commodo
+    congue ante ex consequat lectus. Maecenas nec tellus sed velit convallis
+    laoreet nec sed augue. Etiam a dui mauris. Donec in urna arcu. Interdum et
+    malesuada fames ac ante ipsum primis in faucibus.
+
+    Suspendisse lectus turpis, placerat at consectetur sit amet, tempus eget
+    nibh. Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+    per inceptos himenaeos. Nulla ullamcorper ornare risus. Phasellus rutrum
+    felis nec consequat vestibulum. Curabitur congue turpis in leo pulvinar, ut
+    varius nulla consectetur. Morbi velit urna, ornare sed vestibulum id,
+    lobortis porta turpis. Nulla quis mauris ut felis convallis ornare non et
+    mauris. Aliquam erat volutpat. Integer vehicula tristique nisi nec
+    venenatis.
+
+    Duis non eros placerat, interdum enim id, tincidunt felis. Vestibulum
+    venenatis nec eros convallis condimentum. Donec at velit ultrices, congue
+    orci non, accumsan neque. Vestibulum dapibus viverra nulla a commodo. Mauris
+    volutpat posuere varius. Sed elit enim, congue at porta vitae, iaculis sit
+    amet lectus. Donec ut venenatis metus, vel malesuada nibh. Nullam consequat
+    tincidunt justo, sit amet posuere lorem auctor nec. Vivamus rhoncus
+    tincidunt libero, a dictum risus scelerisque eget. Aliquam erat volutpat.
+    Donec gravida, odio in convallis vehicula, ante tellus pharetra turpis, sit
+    amet lobortis nisl justo varius velit. Ut id magna efficitur, suscipit ex
+    sit amet, ornare augue. Donec in congue arcu. In hac habitasse platea
+    dictumst. Phasellus quam magna, pretium vitae pulvinar sed, dignissim vitae
+    tortor.
+
+    Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+    ridiculus mus. Proin non vestibulum risus, vel ullamcorper ante. Sed varius
+    mollis tellus, quis consectetur ligula. Integer euismod molestie mi in
+    vehicula. Vestibulum quam neque, convallis in tellus eget, feugiat mollis
+    nisi. Morbi tincidunt euismod eleifend. Nunc tincidunt pulvinar lorem non
+    tincidunt. Sed gravida a justo at porta. Pellentesque vehicula ipsum nec
+    magna bibendum ornare. Maecenas consectetur euismod orci a aliquet. Duis
+    varius aliquam ipsum, et eleifend sapien tristique vel. Class aptent taciti
+    sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+    Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam lectus
+    sapien, hendrerit ac tincidunt sed, viverra sit amet ex.
+    */
 }
